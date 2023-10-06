ed0946fcb9132dc7b04aa1404a2875aafcf65fbc
Arif Mamad <marifrahmawan@gmail.com>
2023-10-06 18:59:47 +0800

    feat: solving problem 1

--- 05-Object-and-Array/problem1.ts
+++ 05-Object-and-Array/problem1.ts
@@ -1,11 +1,24 @@
-function compareString(a: string, b: string): string {
-  // your code here
+function compareString(a: string, b: string): string | undefined {
+  let commonSubstring = '';
+  for (let i = 0; i < a.length; i++) {
+    for (let j = 0; j < b.length; j++) {
+      let k = 0;
+      while (a[i + k] === b[j + k]) {
+        k++;
+      }
+      if (k > commonSubstring.length) {
+        return commonSubstring = a.slice(i, i + k);
+      } else {
+        return 'Not Common';
+      }
+    }
+  }
 }
 
-console.log(compareString("AKA", "AKASHI")); // AKA
-console.log(compareString("KANGAROO", "KANG")); // KANG
-console.log(compareString("KI", "KIJANG")); // KI
-console.log(compareString("KUPU-KUPU", "KUPU")); // KUPU
-console.log(compareString("ILALANG", "ILA")); // ILA
+console.log(compareString('AKA', 'AKASHI')); // AKA
+console.log(compareString('KANGAROO', 'KANG')); // KANG
+console.log(compareString('KI', 'KIJANG')); // KI
+console.log(compareString('KUPU-KUPU', 'KUPU')); // KUPU
+console.log(compareString('ILALANG', 'ILA')); // ILA
 
 export default compareString;
