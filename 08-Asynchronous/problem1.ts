45ad8b57a0e714c8edc069be24301e03e5c756db
Arif Mamad <marifrahmawan@gmail.com>
2023-10-12 14:37:28 +0800

    feat: solving problem 1 and 2

--- 08-Asynchronous/problem1.ts
+++ 08-Asynchronous/problem1.ts
@@ -1,9 +1,26 @@
 function jajanBoba(uang: number, callback: (uang: number) => void): void {
-  // your code here
+  console.log('kamu jajan boba dengan harga Rp. 5000');
+
+  if (uang < 5000) {
+    return console.log('Maaf uang kamu belum cukup untuk membeli boba\nSisa uang kamu sebesar ' + uang);
+  }
+
+  setTimeout(() => {
+    console.log('sisa uang kamu Rp. ', uang - 5000);
+    callback(uang - 5000);
+  }, 5000);
 }
 
 function jajanSeblak(uang: number): void {
-  // your code here
+  console.log('kamu jajan seblak dengan harga Rp. 8000');
+
+  if (uang < 8000) {
+    return console.log('Maaf uang kamu belum cukup untuk membeli seblak\nSisa uang kamu sebesar ' + uang);
+  }
+
+  setTimeout(() => {
+    console.log('sisa uang kamu Rp. ', uang - 8000);
+  }, 9000);
 }
 
 jajanBoba(20000, jajanSeblak);
--- 08-Asynchronous/problem3.ts
+++ 08-Asynchronous/problem3.ts
@@ -1,8 +1,21 @@
 function lottery(num: number): Promise<string> {
   // your code here
+  return new Promise<string>((resolve, reject) => {
+    const lot = +(Math.random() * 100).toFixed(0);
+    console.log('undian lotre dimulai...');
+    console.log('sedang mengundi nomor anda...');
+    
+    setTimeout(() => {
+      if (num === lot) {
+        resolve('selamat anda mendapatkan hadiah utama berupa mobil');
+      } else {
+        reject('maaf anda kurang beruntung');
+      }
+    }, 3000);
+  });
 }
 
 lottery(5)
   .then((res) => console.log(res))
   .catch((err) => console.log(err))
-  .finally(() => console.log("undian lotre telah berakhir…"));
+  .finally(() => console.log('undian lotre telah berakhir…'));
