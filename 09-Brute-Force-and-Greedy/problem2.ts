e344ca31883e0ebf08f2872ee67660558487c76b
Arif Mamad <marifrahmawan@gmail.com>
2023-10-13 23:20:40 +0800

    feat: solving problem 2

--- 09-Brute-Force-and-Greedy/problem2.ts
+++ 09-Brute-Force-and-Greedy/problem2.ts
@@ -1,5 +1,22 @@
 function dragonOfLoowater(dragonHead: number[], knightHeight: number[]): number | string {
-  // your code here
+  const sortKnightHeight = knightHeight.sort();
+  const sortDragonHead = dragonHead.sort();
+  let minimal = 0;
+
+  for (let i = 0; i < sortDragonHead.length; i++) {
+    let j = 0;
+    while (j < sortKnightHeight.length && sortKnightHeight[j] < sortDragonHead[i]) {
+      j++;
+    }
+
+    if (j < sortKnightHeight.length) {
+      minimal += sortKnightHeight.splice(j, 1)[0];
+    } else {
+      return 'knight fall';
+    }
+  }
+
+  return minimal;
 }
 
 console.log(dragonOfLoowater([5, 4], [7, 8, 4])); // 11
